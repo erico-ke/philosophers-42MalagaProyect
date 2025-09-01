6d659d12e43af23b58634b42b3294619a60038a7
erico-ke <eliasricokergaravat@gmail.com>
2025-09-01 15:15:07 +0200

    solved forks data race. added -g flag to the Makefile

--- philo/src/mutex.c
+++ philo/src/mutex.c
@@ -6,7 +6,7 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 16:56:59 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/08/29 16:20:54 by erico-ke         ###   ########.fr       */
+/*   Updated: 2025/09/01 15:11:15 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,20 @@ void	fork_mutex_use(t_philo *philo)
 {
 	if (philo->tab->death_flag == 0)
 	{
-		pthread_mutex_lock(philo->r_fork);
-		print_mutex_use(philo, "has taken the right fork");
-		pthread_mutex_lock(philo->l_fork);
-		print_mutex_use(philo, "has taken the left fork");
+		if (philo->id == 0)
+		{
+			pthread_mutex_lock(philo->l_fork);
+			print_mutex_use(philo, "has taken the left fork");
+			pthread_mutex_lock(philo->r_fork);
+			print_mutex_use(philo, "has taken the right fork");
+		}
+		else
+		{
+			pthread_mutex_lock(philo->r_fork);
+			print_mutex_use(philo, "has taken the right fork");
+			pthread_mutex_lock(philo->l_fork);
+			print_mutex_use(philo, "has taken the left fork");
+		}
 		print_mutex_use(philo, "is eating");
 		philo->is_eating = 1;
 		usleep(philo->tab->eat_time * 1000);
