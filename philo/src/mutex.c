28fbf17d92ae7886ec3ee4ba4c7deeeb787002cc
erico-ke <eliasricokergaravat@gmail.com>
2025-09-16 12:18:14 +0200

    norm applied

--- philo/src/mutex.c
+++ philo/src/mutex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mutex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
+/*   By: erico-ke <erico-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 16:56:59 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/09/09 18:26:18 by erico-ke         ###   ########.fr       */
+/*   Updated: 2025/09/16 12:18:03 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,25 @@ void	print_mutex_use(t_philo *philo, char *msg)
 			- philo->tab->starttime, philo->id, msg);
 	pthread_mutex_unlock(philo->tab->writer);
 	pthread_mutex_unlock(philo->tab->death);
+}
 
+static void	fork_mutex_use_aux(t_philo *philo)
+{
+	pthread_mutex_unlock(philo->tab->death);
+	if (philo->id == 0)
+	{
+		pthread_mutex_lock(philo->l_fork);
+		print_mutex_use(philo, "has taken the left fork");
+		pthread_mutex_lock(philo->r_fork);
+		print_mutex_use(philo, "has taken the right fork");
+	}
+	else
+	{
+		pthread_mutex_lock(philo->r_fork);
+		print_mutex_use(philo, "has taken the right fork");
+		pthread_mutex_lock(philo->l_fork);
+		print_mutex_use(philo, "has taken the left fork");
+	}
 }
 
 void	fork_mutex_use(t_philo *philo)
@@ -38,21 +56,7 @@ void	fork_mutex_use(t_philo *philo)
 	pthread_mutex_lock(philo->tab->death);
 	if (philo->tab->death_flag == 0)
 	{
-		pthread_mutex_unlock(philo->tab->death);
-		if (philo->id == 0)
-		{
-			pthread_mutex_lock(philo->l_fork);
-			print_mutex_use(philo, "has taken the left fork");
-			pthread_mutex_lock(philo->r_fork);
-			print_mutex_use(philo, "has taken the right fork");
-		}
-		else
-		{
-			pthread_mutex_lock(philo->r_fork);
-			print_mutex_use(philo, "has taken the right fork");
-			pthread_mutex_lock(philo->l_fork);
-			print_mutex_use(philo, "has taken the left fork");
-		}
+		fork_mutex_use_aux(philo);
 		print_mutex_use(philo, "is eating");
 		philo->is_eating = 1;
 		usleep(philo->tab->eat_time * 1000);
